d748025a621e6b0084019f1cef6f6166f4f88fd2
Kendall Cole <kcole16@gmail.com>
2016-10-17 17:19:40 +0100

    Test

--- public/src/api/index.js
+++ public/src/api/index.js
@@ -1,6 +1,6 @@
 import {configureStore} from '../store/configureStore';
 
-const url_base = 'http://localhost:5000';
+const url_base = 'http://commodityx.5634.flynnhub.com';
 
 const defaultHeaders = {
   'Content-Type': 'application/json',
